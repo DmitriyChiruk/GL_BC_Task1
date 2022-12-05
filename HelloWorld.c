06cf3df6ef8b27a915dcc55af28fb9009a70204f
Dmitriy Chiruk <dchiruk@gmail.com>
2022-12-06 01:55:39 +0200

    Project deployed to the GitHub.

---
+++ HelloWorld.c
@@ -0,0 +1,15 @@
+#include "stdio.h"
+
+int main(){
+    printf("-----Ohayou that`s the Hello World application!-----\n");
+    printf("I`m in the 1st group.\n");
+    printf("My full name:\tChyruk Dmytro\n");
+    printf("Here is my email:\tdchiruk@gmail.com\n");
+    printf("GitHub repository link:\thttps://github.com/DmitriyChiruk/GL_BC_Task1\n");
+    printf("*But actually it`s private, so contact me via email or Slack*\n");
+    printf("GDrive folder: https://drive.google.com/drive/folders/1fFpwTELhp00AJ0rWe64WMlF9NiGDMaqy?usp=sharing\n");
+    
+
+    system("pause");
+    return 0;
+}
